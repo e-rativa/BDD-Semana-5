0f61583f4e3a4af3dd18e84a21002c74d97d8214
dfgoUniandes <df.garciao1@uniandes.edu.co>
2021-05-15 22:12:45 -0500

    Adicion de funcionalidad screenshots esc 16-20

--- CypressBDD/cypress/integration/escenario18.spec.js
+++ CypressBDD/cypress/integration/escenario18.spec.js
@@ -1,27 +1,41 @@
+let count = 0;
 describe('Escenario 18: Login - Cambiar diseño de página web', function () {
     it('Cambio titulo en design', function () {
         cy.visit('https://e61b4ef2fb35.ngrok.io/ghost/#/signin');
+        cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+        cy.wait(1000);
         Login();
         cy.wait(1000);
         cy.visit('https://e61b4ef2fb35.ngrok.io/ghost/#/settings/design');
+        cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+        cy.wait(1000);
         cambiarDesignNameSite();
         cy.wait(1000);
         cy.visit('https://e61b4ef2fb35.ngrok.io/ghost/#/site');
+        cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+        cy.wait(1000);
     });
 });
 
 function Login() {
     cy.get('[id=ember8]').type('pruebasmiso4103@gmail.com', { force: true });
+    cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+    cy.wait(1000);
     cy.get('[id=ember10]').type('qwerty1234', { force: true });
-    cy.wait(500);
+    cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+    cy.wait(1000);
     cy.get('[id=ember12]').click({ force: true });
+    cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
+    cy.wait(1000);
 }
 
 function cambiarDesignNameSite() {
     cy.get('input').then($emberTexts => {
         cy.wrap($emberTexts.get(0)).type('Titulo Grupo', { force: true });
     });
+    cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
     cy.wait(4000);
     cy.get('.view-actions').click();
+    cy.screenshot('Escenario CambiarDiseñoPaginaWeb-'+(count++)+'');
     cy.wait(1000);
 }
